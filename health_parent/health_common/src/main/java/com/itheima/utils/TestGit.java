50d3190d73ee0f3f0970a48b08a5d6e9b204a83c
陈超琦 <lpiggy_@163.com>
2020-04-15 20:50:53 +0800

    测试访问Git

---
+++ health_parent/health_common/src/main/java/com/itheima/utils/TestGit.java
@@ -0,0 +1,16 @@
+package com.itheima.utils;
+
+/**
+ * @author Chan
+ * @TODO
+ * @date 2020/4/15 20:49
+ */
+
+/**
+ * 测试访问Git
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!!!");
+    }
+}
